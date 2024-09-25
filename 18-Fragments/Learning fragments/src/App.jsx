cb58597930f7cf4601d4cdd697f18c6e0c25abf2
nurtaj <nurtajkhanam777@gmail.com>
2024-09-25 20:47:44 +0600

    practicing

--- 18-Fragments/Learning fragments/src/App.jsx
+++ 18-Fragments/Learning fragments/src/App.jsx
@@ -7,17 +7,12 @@ import FoodInput from "./components/FoodInput";
 
 const App = () => {
   let food = ["Seeds", "Vegetables", "Fruits", "Milk", "fish", "Meat", "Nuts"];
-  // let food = [];
-  const handleOnChange = (event) => {
-    const word = event.target.value;
-    console.log(word);
-  };
 
   return (
     <Container>
       <Container>
         <h2 className="food-heading">Healthy Foods</h2>
-        <FoodInput handleOnChange={handleOnChange} />
+        <FoodInput />
       </Container>
       <ConditionRndr items={food} />
       <MapMethod items={food} />
--- 18-Fragments/Learning fragments/src/components/FoodInput.jsx
+++ 18-Fragments/Learning fragments/src/components/FoodInput.jsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
 import styles from "./FoodInput.module.css";
 
-const FoodInput = (handleOnChange) => {
-  // const handleOnChange = (event) => {
-  //   const word = event.target.value;
-  //   console.log(word);
-  // };
+const FoodInput = () => {
+  let [textToShow, setTextState] = useState("");
+  // let textToShow = textStateArr[0];
+  // let setTextState = textStateArr[1];
+  // console.log(`current value of textStateArr: ${textToShow}`);
+
+  const product = (event) => {
+    let p = event.target.value;
+    console.log(p);
+    setTextState(p);
+  };
   return (
     <div>
       <input
         className={styles.container}
         placeholder="Write Your Product"
         type="text"
-        onChange={handleOnChange}
+        onChange={product}
       />
+      <p> {textToShow} </p>
     </div>
   );
 };
